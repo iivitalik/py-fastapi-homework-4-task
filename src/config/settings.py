3b750f6040960c7c2c6760f03dbc14f9a61914aa
msi <lg.mobile0808@gmail.com>
2025-12-23 20:55:39 +0200

    updates docker-compose

--- src/config/settings.py
+++ src/config/settings.py
@@ -18,12 +18,12 @@ class BaseAppSettings(BaseSettings):
 
     LOGIN_TIME_DAYS: int = 7
 
-    EMAIL_HOST: str = os.getenv("EMAIL_HOST", "mailhog")
-    EMAIL_PORT: int = int(os.getenv("EMAIL_PORT", 1025))
-    EMAIL_HOST_USER: str = os.getenv("EMAIL_HOST_USER", "testuser@mate.com")
+    EMAIL_HOST: str = os.getenv("EMAIL_HOST", "host")
+    EMAIL_PORT: int = int(os.getenv("EMAIL_PORT", 25))
+    EMAIL_HOST_USER: str = os.getenv("EMAIL_HOST_USER", "testuser")
     EMAIL_HOST_PASSWORD: str = os.getenv("EMAIL_HOST_PASSWORD", "test_password")
     EMAIL_USE_TLS: bool = os.getenv("EMAIL_USE_TLS", "False").lower() == "true"
-    MAILHOG_API_PORT: int = int(os.getenv("MAILHOG_API_PORT", 8025))
+    MAILHOG_API_PORT: int = os.getenv("MAILHOG_API_PORT", 8025)
 
     S3_STORAGE_HOST: str = os.getenv("MINIO_HOST", "minio-theater")
     S3_STORAGE_PORT: int = os.getenv("MINIO_PORT", 9000)
