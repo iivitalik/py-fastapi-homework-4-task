51a70f10d516c01c0707aaeaee97e0861ee5a4bb
msi <lg.mobile0808@gmail.com>
2025-12-23 20:20:20 +0200

    sollution

--- src/config/settings.py
+++ src/config/settings.py
@@ -18,12 +18,12 @@ class BaseAppSettings(BaseSettings):
 
     LOGIN_TIME_DAYS: int = 7
 
-    EMAIL_HOST: str = os.getenv("EMAIL_HOST", "host")
-    EMAIL_PORT: int = int(os.getenv("EMAIL_PORT", 25))
-    EMAIL_HOST_USER: str = os.getenv("EMAIL_HOST_USER", "testuser")
+    EMAIL_HOST: str = os.getenv("EMAIL_HOST", "mailhog")
+    EMAIL_PORT: int = int(os.getenv("EMAIL_PORT", 1025))
+    EMAIL_HOST_USER: str = os.getenv("EMAIL_HOST_USER", "testuser@mate.com")
     EMAIL_HOST_PASSWORD: str = os.getenv("EMAIL_HOST_PASSWORD", "test_password")
     EMAIL_USE_TLS: bool = os.getenv("EMAIL_USE_TLS", "False").lower() == "true"
-    MAILHOG_API_PORT: int = os.getenv("MAILHOG_API_PORT", 8025)
+    MAILHOG_API_PORT: int = int(os.getenv("MAILHOG_API_PORT", 8025))
 
     S3_STORAGE_HOST: str = os.getenv("MINIO_HOST", "minio-theater")
     S3_STORAGE_PORT: int = os.getenv("MINIO_PORT", 9000)
@@ -43,8 +43,8 @@ class Settings(BaseAppSettings):
     POSTGRES_DB_PORT: int = int(os.getenv("POSTGRES_DB_PORT", 5432))
     POSTGRES_DB: str = os.getenv("POSTGRES_DB", "test_db")
 
-    SECRET_KEY_ACCESS: str = os.getenv("SECRET_KEY_ACCESS", os.urandom(32))
-    SECRET_KEY_REFRESH: str = os.getenv("SECRET_KEY_REFRESH", os.urandom(32))
+    SECRET_KEY_ACCESS: str = os.getenv("SECRET_KEY_ACCESS", "default_access_key")
+    SECRET_KEY_REFRESH: str = os.getenv("SECRET_KEY_REFRESH", "default_refresh_key")
     JWT_SIGNING_ALGORITHM: str = os.getenv("JWT_SIGNING_ALGORITHM", "HS256")
 
 
